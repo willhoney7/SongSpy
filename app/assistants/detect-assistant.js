ba049a031d4a7c49215726664e5454dbc1d46a78
Tibfib <Tibfib1@gmail.com>
2011-06-24 21:27:26 -0500

    App Shell

---
+++ app/assistants/detect-assistant.js
@@ -0,0 +1,17 @@
+function DetectAssistant() {}
+
+DetectAssistant.prototype.setup = function() {
+
+};
+
+DetectAssistant.prototype.activate = function(event) {
+
+};
+
+DetectAssistant.prototype.deactivate = function(event) {
+
+};
+
+DetectAssistant.prototype.cleanup = function(event) {
+
+};
