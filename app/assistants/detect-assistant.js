0dd9b473fc6d5bdf7eeddb16ca0dc7815f76c78b
Tibfib <Tibfib1@gmail.com>
2011-06-27 19:18:37 -0500

    Pretty much ready for plugin integration

--- app/assistants/detect-assistant.js
+++ app/assistants/detect-assistant.js
@@ -1,25 +1,15 @@
 function DetectAssistant() {}
 
 DetectAssistant.prototype.setup = function() {
-	
-	this.gctx = document.getElementById('waveform-canvas').getContext('2d');
-	
-	/*this.controller.setupWidget("recordButton", 
-		{},
-    	this.recordModel = {
-			label : "Go!",
-         	disabled: false,
-		 	buttonClass: 'affirmative'
-     	}
-    );*/
-   /* this.controller.setupWidget("spinner",
+		
+    this.controller.setupWidget("spinner",
 		{
 	    	spinnerSize: "large"
 	  	},
 	  	this.spinnerModel = {
 			spinning: false
 	  	}
-	); */
+	);
 
 	this.controller.setupWidget("playButton",
         {
@@ -64,7 +54,6 @@ DetectAssistant.prototype.deactivate = function(event) {
 	this.controller.stopListening(this.controller.get('playButton'), Mojo.Event.tap, this.playCallback);
 	this.controller.stopListening(this.controller.get('recordButton'), Mojo.Event.tap, this.startCallback);
     //this.audioRecorder.removeEventListener("error", this.handleError, false);
-	this.stopGrapher();
 };
 
 DetectAssistant.prototype.cleanup = function(event) {
@@ -75,16 +64,15 @@ DetectAssistant.prototype.startCallback = function(event) {
 		this.isWorking = true;
 
 		this.randomInt = Math.floor(Math.random()*99999);
-		this.controller.get('area-to-update1').innerText = 'Recording ...';
+
 		this.audioRecorder.startAudioCapture(/*Mojo.appPath +*/ "/media/internal/audio/recording" + this.randomInt + ".wav", 
 			{audioCaptureFormat: this.getAudioCaptureFormat()}
 		);
 
-		setTimeout(this.stopCallback.bind(this), 3000);
-		//new Effect.Pulsate(this.controller.get('recordButton'), {duration: 10});
-		
-		this.startGrapher();
+		setTimeout(this.stopCallback.bind(this), 8000);
 
+		this.animateText("Listening");
+		
 		this.playModel.disabled = true;
 		this.controller.modelChanged(this.playModel);
 
@@ -92,46 +80,32 @@ DetectAssistant.prototype.startCallback = function(event) {
 
 	}
 
-	//this.recordModel.disabled = true;
-	//this.recordModel.label = "Analyzing...";
-	//this.controller.modelChanged(this.recordModel);
-	
-	//this.controller.get('spinner').mojo.start();
+	this.controller.get('spinner').mojo.start();
 
 };
 
 DetectAssistant.prototype.stopCallback = function(event) {
-
-	this.controller.get('area-to-update1').innerText = 'Recording finished, press play';
-	//this.controller.get('spinner').mojo.stop();
 	
 	this.playModel.disabled = false;
 	this.controller.modelChanged(this.playModel);
 
-	//this.recordModel.disabled = false;
-	//this.recordModel.label = "Go!";
-	//this.controller.modelChanged(this.recordModel);
-
 	this.controller.get("recordButton").removeClassName("recording");
-	this.controller.get("recordButton").addClassName("analyzing");
-	this.controller.get('waveform-canvas').hide();
-	this.controller.get("text-update").show();
-	this.controller.get("text-update").innerHTML = "Analyzing";
-	this.startAnimatingTextUpdate(this.controller.get("text-update"));
+	this.controller.get("recordButton").addClassName("analyzing");	
+	this.stopAnimatingTextUpdate();
+	this.animateText("Analyzing");
 
 	this.audioRecorder.stopAudioCapture();
-	this.stopGrapher();
-
 
 	//var filePath = "/media/internal/audio/recording" + this.randomInt +  ".wav";
 	//@TODO: Send file to plugin
-	setTimeout(this.gotResponse.bind(this), 3000);
+	setTimeout(this.gotResponse.bind(this), 8000);
 
 };
 
 DetectAssistant.prototype.gotResponse = function(transport){
 
 	this.isWorking = false;
+	this.controller.get('spinner').mojo.stop();
 
 	var transport = {
 	    response: {
@@ -167,7 +141,7 @@ DetectAssistant.prototype.gotResponse = function(transport){
 	            },
 	            {
 	                score: 25,
-	                title: "Accidents Might Happen",
+	                title: "Some say...",
 	                message: "OK (match type 6)",
 	                artist_id: "AROCZUC1187B9AD05B",
 	                artist_name: "My Grandma",
@@ -183,68 +157,38 @@ DetectAssistant.prototype.gotResponse = function(transport){
 	}
 
 	this.controller.get("recordButton").removeClassName("analyzing");
-	this.controller.get('waveform-canvas').show();
-	this.controller.get("text-update").hide();
+
 	this.stopAnimatingTextUpdate();
 
 }
+DetectAssistant.prototype.animateText = function(text){
+	var div = this.controller.get("text-update");
+		div.innerHTML = text;
+		div.show();
 
-DetectAssistant.prototype.startAnimatingTextUpdate = function(div){
-	var innerHTML = div.innerHTML;
 	this.animateTextUpdate = true;
 
 	function addDot(){
 		if(this.animateTextUpdate){
 			div.innerHTML += ".";
-			setTimeout(addDot.bind(this), 500);
-			if(div.innerHTML.length - 4 === innerHTML.length){
-				div.innerHTML = innerHTML;
+			this.t = setTimeout(addDot.bind(this), 300);
+			if(div.innerHTML.length - 4 === text.length){
+				div.innerHTML = text;
 			}	
 		} else {
-			div.innerHTML = innerHTML;
+			div.innerHTML = text;
 		}
 	}	
 
-	setTimeout(addDot.bind(this), 500);
+	this.t = setTimeout(addDot.bind(this), 300);
 }
 
 DetectAssistant.prototype.stopAnimatingTextUpdate = function(div){
 	this.animateTextUpdate = false;
-}
+	this.controller.get("text-update").hide();
+	clearTimeout(this.t);
 
-DetectAssistant.prototype.grapher = function(){
-	// Get most recent sample of VU data.
-	if (0 != this.audioRecorder.vuData.length){
-		var vuData = this.audioRecorder.vuData.pop();
-		var height = 80 * vuData.peak[0];
-
-		if (this.gctx){
-			this.gctx.fillStyle = "rgb(0, 200, 0)";//getGraphColor(this.audioRecorder.audioCapture);
-			this.gctx.fillRect(this.x, 80-height, 4, 80);
-		}
-	}
-
-	this.x+=5;
-	if (this.x == 300) {
-		this.gctx.fillStyle = "rgba(0, 0, 0, 0)"
-		this.gctx.clearRect(0, 0, 300, 80);
-		this.x = 0;
-	}
-}	
-
-DetectAssistant.prototype.startGrapher = function(){	
-	this.x = 0
-	this.gctx.fillStyle = "rgba(0, 0, 0, 0)"
-	this.gctx.clearRect(0, 0, 300, 80);
-	this.gtimer = window.setInterval(this.grapher.bind(this), 200);
-	
-}
-
-DetectAssistant.prototype.stopGrapher= function(){
-	if (this.gtimer){
-		window.clearInterval(this.gtimer);
-		this.gtimer = 0;
-	}
+	//this.audioRecorder.vuData
 }
 
 DetectAssistant.prototype.playCallback = function(event) {
